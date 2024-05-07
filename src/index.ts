59270e51f33eb22f4556455c7d1db656d14262e2
Kasun Vithanage <alankasun@gmail.com>
2024-05-07 14:48:48 +0530

    feat: add some validation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import axios, { AxiosInstance } from "axios";
+
 export interface EasyDebridOptions {
   apiUrl: string;
   clientId: string;
@@ -5,5 +7,25 @@ export interface EasyDebridOptions {
 }
 
 export class EasyDebrid {
-  constructor(private readonly options: EasyDebridOptions) {}
+  private apiClient: AxiosInstance;
+  constructor(private readonly options: EasyDebridOptions) {
+    if (!options.apiUrl) {
+      throw new Error("apiUrl is required");
+    }
+
+    if (!options.clientId) {
+      throw new Error("clientId is required");
+    }
+
+    if (!options.clientSecret) {
+      throw new Error("clientSecret is required");
+    }
+
+    this.apiClient = axios.create({
+      baseURL: options.apiUrl,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
 }
