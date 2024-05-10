0a6b9906dd1b90a609b4542038d98fc63fd2e466
Kasun Vithanage <alankasun@gmail.com>
2024-05-10 14:58:54 +0530

    fix: tsup entry

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   target: "es2020",
+  entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   splitting: false,
   sourcemap: true,
