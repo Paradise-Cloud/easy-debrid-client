ddb396663820c9514f0a8fb5a40aef07d5f922e9
Rasika Lakmal Dassanyaka <drasikalakmal@gmail.com>
2024-06-13 18:18:51 +0530

    feat: ed- merchant , wip: testing

---
+++ src/merchant.ts
@@ -0,0 +1,119 @@
+import axios, { AxiosInstance } from "axios";
+import { toEasyDebridError } from "./errors";
+
+export interface EasyDebridGetResellerPricesResponse {
+  prices: Price[];
+}
+
+export interface Price {
+  price: number;
+  days: number;
+}
+
+export interface EasyDebridTopupResellerBalanceResponse {
+  url: string;
+}
+
+export interface EasyDebridGetResellerBalanceResponse {
+  balance: number;
+}
+
+export interface EasyDebridGenerateResellerCouponResponse {
+  days: number;
+}
+
+export interface EasyDebridListResellerCouponsResponse {
+  coupons: Coupon[];
+}
+
+export interface Coupon {
+  coupon: string;
+  days: number;
+  created: string;
+  expires: string;
+}
+
+export interface EasyDebridMerchantOptions {
+  accessToken?: string;
+  env?: "production" | "sandbox";
+}
+
+export class EasyDebridMerchant {
+  private apiClient: AxiosInstance;
+  constructor(private readonly options: EasyDebridMerchantOptions) {
+    options.env = options.env || "production";
+    const baseUrl =
+      options.env === "production"
+        ? "https://api.easydebrid.com/api/v1"
+        : "https://dev.easydebrid.com/api/v1";
+    this.apiClient = axios.create({
+      baseURL: baseUrl,
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${this.options.accessToken}`,
+      },
+    });
+  }
+
+  async getResellerPrices() {
+    try {
+      const { data } =
+        await this.apiClient.get<EasyDebridGetResellerPricesResponse>(
+          "/reseller/prices",
+        );
+      return data;
+    } catch (error) {
+      return toEasyDebridError(error);
+    }
+  }
+
+  async topupResellerBalance(amount: number) {
+    try {
+      const { data } =
+        await this.apiClient.post<EasyDebridTopupResellerBalanceResponse>(
+          "/reseller/topup",
+          { amount },
+        );
+      return data;
+    } catch (error) {
+      return toEasyDebridError(error);
+    }
+  }
+
+  async getResellerBalance() {
+    try {
+      const { data } =
+        await this.apiClient.get<EasyDebridGetResellerBalanceResponse>(
+          "/reseller/balance",
+        );
+      return data;
+    } catch (error) {
+      return toEasyDebridError(error);
+    }
+  }
+
+  async generateResellerCoupon(days: number) {
+    try {
+      const { data } =
+        await this.apiClient.post<EasyDebridGenerateResellerCouponResponse>(
+          "/reseller/generatecoupon",
+          { days },
+        );
+      return data;
+    } catch (error) {
+      return toEasyDebridError(error);
+    }
+  }
+
+  async listResellerCoupons() {
+    try {
+      const { data } =
+        await this.apiClient.get<EasyDebridListResellerCouponsResponse>(
+          "/reseller/prices",
+        );
+      return data;
+    } catch (error) {
+      return toEasyDebridError(error);
+    }
+  }
+}
